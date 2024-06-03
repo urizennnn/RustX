c4a945077267ad155852c15bfa9b2d91fb98a25b
urizennnn <igamerryt@gmail.com>
2024-06-03 15:22:40 +0100

    chore:baby steps

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+extern crate gtk;
+use gtk::prelude::*;
+use gtk::{Button, Window, WindowType};
+
+fn main() {
+    gtk::init().expect("Failed to initialize GTK.");
+
+    let window = Window::new(WindowType::Toplevel);
+    window.set_title("Hello, GTK+");
+    window.set_default_size(350, 70);
+
+    let button = Button::with_label("Click me!");
+    button.connect_clicked(|_| {
+        println!("Button clicked!");
+    });
+
+    window.add(&button);
+    window.show_all();
+
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
+
+    gtk::main();
+}
