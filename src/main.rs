38d5577425a104aa63458b8cbc0aa175a28b0779
urizennnn <igamerryt@gmail.com>
2024-06-13 23:24:52 +0100

    chore:learnt how to make switches

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,66 @@
-// use std::cell::Cell;
-// use std::rc::Rc;
-//
-// use glib::clone;
-// use gtk::prelude::*;
-// use gtk::{self, glib, Application, ApplicationWindow, Button, Orientation};
-//
-// const APP_ID: &str = "org.gtk_rs.GObjectMemoryManagement5";
-//
-// fn main() -> glib::ExitCode {
-//     // Create a new application
-//     let app = Application::builder().application_id(APP_ID).build();
-//
-//     // Connect to "activate" signal of `app`
-//     app.connect_activate(build_ui);
-//
-//     // Run the application
-//     app.run()
-// }
-//
-// fn build_ui(app: &Application) {
-//     // Create two buttons
-//     let button_increase = Button::builder()
-//         .label("Increase")
-//         .margin_top(12)
-//         .margin_bottom(12)
-//         .margin_start(12)
-//         .margin_end(12)
-//         .build();
-//     let button_decrease = Button::builder()
-//         .label("Decrease")
-//         .margin_top(12)
-//         .margin_bottom(12)
-//         .margin_start(12)
-//         .margin_end(12)
-//         .build();
-//
-//     // Reference-counted object with inner mutability
-//     let number = Rc::new(Cell::new(0));
-//
-//     // Connect callbacks
-//     // When a button is clicked, `number` and label of the other button will be changed
-//     button_increase.connect_clicked(clone!(@weak number, @weak button_decrease =>
-//         move |_| {
-//             number.set(number.get() + 1);
-//             button_decrease.set_label(&number.get().to_string());
-//     }));
-//     button_decrease.connect_clicked(clone!(@weak button_increase =>
-//         move |_| {
-//             number.set(number.get() - 1);
-//             button_increase.set_label(&number.get().to_string());
-//     }));
-//
-//     // Add buttons to `gtk_box`
-//     let gtk_box = gtk::Box::builder()
-//         .orientation(Orientation::Vertical)
-//         .build();
-//     gtk_box.append(&button_increase);
-//     gtk_box.append(&button_decrease);
-//
-//     // Create a window
-//     let window = ApplicationWindow::builder()
-//         .application(app)
-//         .title("My GTK App")
-//         .child(&gtk_box)
-//         .build();
-//
-//     // Present the window
-//     window.present();
-// }
+use gtk::prelude::*;
+use gtk::{glib, Align, Application, ApplicationWindow, Box, Orientation, Switch};
 
-use gtk::prelude::ToValue;
+const APP_ID: &str = "org.gtk_rs.GObjectProperties1";
 
-fn main() {
-    let integer = 5.to_value();
-    println!("{:?}", integer);
+fn main() -> glib::ExitCode {
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    // Create the switch
+    let switch = Switch::new();
+    let switch2 = Switch::new();
+    // Set and then immediately obtain active property
+    switch.set_active(true);
+    switch2.set_active(true);
+    switch.connect_activate(|switch| {
+        switch.set_active(false);
+        let switch_active = switch.is_active();
+        println!("The active property of switch is {}", switch_active);
+    });
+    let switch_active = switch.is_active();
+    switch
+        .bind_property("active", &switch2, "active")
+        .transform_to(|_, active: bool| {
+            print!("The active property of switch is {}", active);
+            Some(!active)
+        })
+        .transform_to(|_, active: bool| Some(active))
+        .sync_create()
+        .build();
+
+    // This prints: "The active property of switch is true"
+    println!("The active property of switch is {}", switch_active);
+
+    // Set up box
+    let gtk_box = Box::builder()
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .valign(Align::Center)
+        .halign(Align::Center)
+        .spacing(12)
+        .orientation(Orientation::Vertical)
+        .build();
+    gtk_box.append(&switch);
+    gtk_box.append(&switch2);
+
+    // Create a window
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&gtk_box)
+        .build();
+
+    // Present the window
+    window.present();
 }
